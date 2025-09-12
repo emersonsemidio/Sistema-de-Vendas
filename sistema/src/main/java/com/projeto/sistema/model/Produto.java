bfe11e36dd05b275fc1305058c398ef5759bebf1
Emerson Emidio <emersons.emidio01@gmail.com>
2025-09-12 03:37:57 -0300

    Mudança no Model de Produto retirando as validações

--- sistema/src/main/java/com/projeto/sistema/model/Produto.java
+++ sistema/src/main/java/com/projeto/sistema/model/Produto.java
@@ -15,28 +15,11 @@ public class Produto {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-
-  @NotBlank(message = "O campo é obrigatorio")
-  @Size(min = 3, max = 50, message = "O nome do produto deve ter entre 3 e 50 caracteres")
   private String nome;
-
-  @NotBlank(message = "O campo é obrigatorio")
-  @Size(min = 10, max = 200, message = "A descrição deve ter entre 10 e 200 caracteres")
   private String descricao;
-
-  @NotBlank(message = "O campo é obrigatorio")
-  @Positive(message = "O campo deve ser um número positivo")
   private Double preco;
-
-  @NotBlank(message = "O campo é obrigatorio")
-  @Positive(message = "O campo deve ser um número positivo")
   private Integer quantidade;
-
-  @NotBlank(message = "O campo é obrigatorio")
-  @Size(min = 5, max = 100, message = "A URL da imagem deve ter entre 5 e 100 caracteres")
   private String imagemUrl;
-
-  
   private String usuarioId;
 
   // Getters and Setters
