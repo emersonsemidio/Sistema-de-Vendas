cbc3bfd883842278df95e9d8b4b0aa8f8bf39337
Emerson Emidio <emersons.emidio01@gmail.com>
2025-09-12 03:34:49 -0300

    Criação do ProdutoRegisterDto com suas validações

---
+++ sistema/src/main/java/com/projeto/sistema/dto/ProdutoRegisterDto.java
@@ -0,0 +1,81 @@
+package com.projeto.sistema.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Positive;
+
+public class ProdutoRegisterDto {
+
+  @NotBlank(message = "O campo é obrigatorio")
+  @Size(min = 3, max = 50, message = "O nome do produto deve ter entre 3 e 50 caracteres")
+  private String nome;
+
+  @NotBlank(message = "O campo é obrigatorio")
+  @Size(min = 10, max = 200, message = "A descrição deve ter entre 10 e 200 caracteres")
+  private String descricao;
+
+  @NotBlank(message = "O campo é obrigatorio")
+  @Positive(message = "O campo deve ser um número positivo")
+  private Double preco;
+
+  @NotBlank(message = "O campo é obrigatorio")
+  @Positive(message = "O campo deve ser um número positivo")
+  private Integer quantidade;
+
+  @NotBlank(message = "O campo é obrigatorio")
+  @Size(min = 5, max = 100, message = "A URL da imagem deve ter entre 5 e 100 caracteres")
+  private String imagemUrl;
+
+  
+  private String usuarioId;
+
+  // Getters and Setters
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getDescricao() {
+    return descricao;
+  }
+
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
+  }
+
+  public Double getPreco() {
+    return preco;
+  }
+
+  public void setPreco(Double preco) {
+    this.preco = preco;
+  }
+
+  public Integer getQuantidade() {
+    return quantidade;
+  }
+
+  public void setQuantidade(Integer quantidade) {
+    this.quantidade = quantidade;
+  }
+
+  public String getImagemUrl() {
+    return imagemUrl;
+  }
+
+  public void setImagemUrl(String imagemUrl) {
+    this.imagemUrl = imagemUrl;
+  }
+
+  public String getUsuarioId() {
+    return usuarioId;
+  }
+
+  public void setUsuarioId(String usuarioId) {
+    this.usuarioId = usuarioId;
+  }
+
+}
